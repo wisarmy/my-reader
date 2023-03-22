cc5fd79297b0d275a68a988235118a73d0e2b1b8
gaomingjun <wisarmy@gmail.com>
2023-03-22 17:36:39 +0800

    Add router

---
+++ src/router/router.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Main from "../views/Main.vue";
+import Config from "../views/Config.vue";
+
+const routes = [
+    { path: "/", component: Main },
+    { path: "/config", component: Config },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
