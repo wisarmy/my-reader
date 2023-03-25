a54367c86f1ddb198a499ac0aa78638753e75077
gaomingjun <wisarmy@gmail.com>
2023-03-25 10:14:14 +0800

    Optimize header select style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -37,25 +37,60 @@
           />
         </svg>
       </div>
-      <div class="relative">
-        <select
-          class="block appearance-none w-full bg-gray-100 border border-gray-200 text-xs text-black py-1 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-gray-100 focus:border-gray-100"
-        >
-          <option>选项1</option>
-          <option>选项2</option>
-          <option>选项3</option>
-        </select>
-        <div
-          class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-1 ml-1 text-gray-400"
-        >
-          <svg
-            class="fill-current h-4 w-4"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 12l-6-6h12z" />
-          </svg>
-        </div>
+      <div class="relative w-24">
+        <Listbox v-model="selectedPerson">
+          <div class="relative mt-1">
+            <ListboxButton
+              class="relative w-full h-5 cursor-defaule rounded-md bg-gray-100 pl-3 pr-10 text-left text-gray-500 shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+            >
+              <span class="block truncate">{{ selectedPerson.name }}</span>
+              <span
+                class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+              >
+                <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+              </span>
+            </ListboxButton>
+
+            <transition
+              leave-active-class="transition duration-100 ease-in"
+              leave-from-class="opacity-100"
+              leave-to-class="opacity-0"
+            >
+              <ListboxOptions
+                class="absolute mt-1 max-h-60 min-w-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+              >
+                <ListboxOption
+                  v-slot="{ active, selected }"
+                  v-for="person in people"
+                  :key="person.name"
+                  :value="person"
+                  as="template"
+                >
+                  <li
+                    :class="[
+                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900',
+                      'relative cursor-default select-none py-2 pl-10 pr-4',
+                    ]"
+                  >
+                    <span
+                      :class="[
+                        selected ? 'font-medium' : 'font-normal',
+                        'block truncate',
+                      ]"
+                      >{{ person.name }}</span
+                    >
+                    <span
+                      v-if="selected"
+                      class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                    >
+                      <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                    </span>
+                  </li>
+                </ListboxOption>
+              </ListboxOptions>
+            </transition>
+          </div>
+        </Listbox>
       </div>
       <!-- 上传 -->
       <div class="relative bg-gray-100 text-black">
@@ -102,7 +137,18 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+import {
+  Listbox,
+  ListboxLabel,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from "@headlessui/vue";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/vue/20/solid";
 import { writeBinaryFile, BaseDirectory, readBinaryFile } from "@tauri-apps/api/fs";
+const people = [{ name: "最近" }, { name: "标题" }, { name: "作者" }];
+const selectedPerson = ref(people[0]);
 function handleFileSelect(): void {
   const fileInput = document.createElement("input");
   fileInput.type = "file";
