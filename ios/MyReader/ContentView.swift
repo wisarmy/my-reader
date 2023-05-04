5bc2131f1a1350631e0d43fee0f6a240996e3d11
wisarmy <wisarmy@gmail.com>
2023-05-04 17:35:58 +0800

    add ios

---
+++ ios/MyReader/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyReader
+//
+//  Created by 高明军 on 2023/5/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/MyReader/MyReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyReaderApp.swift
+//  MyReader
+//
+//  Created by 高明军 on 2023/5/4.
+//
+
+import SwiftUI
+
+@main
+struct MyReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
